93db7bd02065c44988b38170f615a2fbee907089
Xavier <xavier2696@gmail.com>
2018-11-05 23:25:23 +0800

    added mcts to groupai.py file, works but still needs to investigate bugs

--- ai/group_23.py
+++ ai/group_23.py
@@ -1,25 +1,250 @@
 from goboard import Player, BoardInfo, GuiManager
+import random
+from math import *
 
 
 class Ai(Player):
-    def __init__(self, color):
+    def __init__(self, color, **kwargs):
         super(Ai, self).__init__(color)
+        self.state = GomokuState()
+        self.state.color = color
+        try:
+            size_x, size_y = kwargs['board_size']
+            self.state.n = size_x
+        except IndexError:
+            self.state.n = 13
 
     def get_action(self, board: BoardInfo) -> (int, int):
-        """
-            Implement your algorithm here.
+        self.state.update_with_board_info(board)
+        move = UCT(rootstate=self.state, itermax=100, verbose=False)
+        print("Best Move: " + str(move))
+        possible_x = int(move / self.state.n)
+        possible_y = int(move % self.state.n)
+        if board.is_legal_action(possible_x, possible_y):
+            print("Move possible: %d,%d" % (possible_x, possible_y))
+            return possible_x, possible_y
+        else:
+            print("Move not possible: %d,%d" % (possible_x, possible_y))
+            # default to easy ai for now, need another solution
+            for x in range(0, board.size_x):
+                for y in range(0, board.size_y):
+                    if board.is_legal_action(x, y):
+                        return x, y
+                    else:
+                        continue
 
-            **Important**
-            1. You must return (x, y)
-            2. If any exception is raised, you will lose the game directly. Use try-except to handle error/exception.
-            3. To get current state of the game, you could call board.dense or board.steps to get data.
 
-            :return: int x, int y
-            """
+class GomokuState:
 
-        for x in range(0, board.size_x):
-            for y in range(0, board.size_y):
-                if board.is_legal_action(x, y):
-                    return x, y
+    def update_with_board_info(self, board_info):
+        self.board = [0 for element in range(self.n * self.n)]
+        for x in range(0, board_info.size_x):
+            for y in range(0, board_info.size_y):
+                if board_info.is_black(x, y):
+                    if self.color == "black":
+                        self.board[int(x * self.n) + y] = 1
+                    else:
+                        self.board[int(x * self.n) + y] = 2
                 else:
-                    continue
+                    if board_info.is_white(x, y):
+                        if self.color == "white":
+                            self.board[int(y * self.n) + x] = 1
+                        else:
+                            self.board[int(y * self.n) + x] = 2
+        # print(self)
+
+    def __init__(self):
+        self.color = "white"
+        self.playerJustMoved = 2  # At the root pretend the player just moved is p2 - p1 has the first move
+        self.n = 13  # board size
+        self.board = [0 for element in range(self.n * self.n)]  # 0 = empty, 1 = player 1, 2 = player 2
+        self.win_positions = []
+        self.win_positions += self.generate_diagonal_win_positions()
+        self.win_positions += self.generate_horizontal_win_positions()
+        self.win_positions += self.generate_vertical_win_positions()
+
+    def Clone(self):
+        """ Create a deep clone of this game state.
+        """
+        st = GomokuState()
+        st.playerJustMoved = self.playerJustMoved
+        st.board = self.board[:]
+        st.n = self.n
+        st.win_positions = self.win_positions
+        st.color = self.color
+        return st
+
+    def DoMove(self, move):
+        """ Update a state by carrying out the given move.
+            Must update playerToMove.
+        """
+        assert move >= 0 and move < (self.n * self.n) and move == int(move) and self.board[move] == 0
+        self.playerJustMoved = 3 - self.playerJustMoved
+        self.board[move] = self.playerJustMoved
+
+    def GetMoves(self):
+        """ Get all possible moves from this state.
+        """
+        # if self.player_has_won():
+        # print(self)
+        # return []
+        return [i for i in range(self.n * self.n) if self.board[i] == 0]
+
+    def GetResult(self, playerjm):
+        """ Get the game result from the viewpoint of playerjm.
+        """
+        for (v, w, x, y, z) in self.win_positions:
+            if self.board[v] == self.board[w] == self.board[x] == self.board[y] == self.board[z]:
+                if self.board[v] == playerjm:
+                    return 1.0
+                else:
+                    return 0.0
+        if self.GetMoves() == []: return 0.5  # draw
+        assert False  # Should not be possible to get here
+
+    def player_has_won(self):
+        for (v, w, x, y, z) in self.win_positions:
+            if (self.board[v] == 1 or self.board[v] == 2) and (
+                    self.board[v] == self.board[w] == self.board[x] == self.board[y] == self.board[z]):
+                return True
+        return False
+
+    def generate_horizontal_win_positions(self):
+        h_win_positions = []
+        for i in range(0, (self.n * self.n)):
+            if ((i % self.n) < self.n) and (((i % self.n) + 5) < self.n):
+                h_win_positions += [(i, i + 1, i + 2, i + 3, i + 4)]
+        return h_win_positions
+
+    def generate_vertical_win_positions(self):
+        v_win_positions = []
+        for i in range(0, (self.n * self.n)):
+            if ((i % self.n) < self.n) and ((i + 5 * self.n) < self.n * self.n):
+                v_win_positions += [(i, i + 1 * self.n, i + 2 * self.n, i + 3 * self.n, i + 4 * self.n)]
+        return v_win_positions
+
+    def generate_diagonal_win_positions(self):
+        d_win_positions = []
+        n = self.n
+        for i in range(0, (self.n * self.n)):
+            if ((i % n) < n) and ((i + 4 * n + 4) <= (n * n - 1) and (i % n) + 4 < n):
+                d_win_positions += [(i, i + 1 * n + 1, i + 2 * n + 2, i + 3 * n + 3, i + 4 * n + 4)]
+            if ((i % n) < n) and ((i + 4 * n - 4) <= (n * n - 1) and (i % n) - 4 >= 0):
+                d_win_positions += [(i, i + 1 * n - 1, i + 2 * n - 2, i + 3 * n - 3, i + 4 * n - 4)]
+        return d_win_positions
+
+    def __repr__(self):
+        s = ""
+        for i in range(self.n * self.n):
+            if self.board[i] == 0:
+                s += '[ ]'
+            else:
+                if self.board[i] == 1:
+                    s += '[1]'
+                else:
+                    if self.board[i] == 2:
+                        s += '[2]'
+            if i % self.n == self.n - 1:
+                s += '\n'
+        return s
+
+
+class Node:
+    """ A node in the game tree. Note wins is always from the viewpoint of playerJustMoved.
+        Crashes if state not specified.
+    """
+
+    def __init__(self, move=None, parent=None, state=None):
+        self.move = move  # the move that got us to this node - "None" for the root node
+        self.parentNode = parent  # "None" for the root node
+        self.childNodes = []
+        self.wins = 0
+        self.visits = 0
+        self.untriedMoves = state.GetMoves()  # future child nodes
+        self.playerJustMoved = state.playerJustMoved  # the only part of the state that the Node needs later
+
+    def UCTSelectChild(self):
+        """ Use the UCB1 formula to select a child node. Often a constant UCTK is applied so we have
+            lambda c: c.wins/c.visits + UCTK * sqrt(2*log(self.visits)/c.visits to vary the amount of
+            exploration versus exploitation.
+        """
+        s = sorted(self.childNodes, key=lambda c: c.wins / c.visits + sqrt(2 * log(self.visits) / c.visits))[-1]
+        return s
+
+    def AddChild(self, m, s):
+        """ Remove m from untriedMoves and add a new child node for this move.
+            Return the added child node
+        """
+        n = Node(move=m, parent=self, state=s)
+        self.untriedMoves.remove(m)
+        self.childNodes.append(n)
+        return n
+
+    def Update(self, result):
+        """ Update this node - one additional visit and result additional wins. result must be from the viewpoint of playerJustmoved.
+        """
+        self.visits += 1
+        self.wins += result
+
+    def __repr__(self):
+        return "[M:" + str(self.move) + " W/V:" + str(self.wins) + "/" + str(self.visits) + " U:" + str(
+            self.untriedMoves) + "]"
+
+    def TreeToString(self, indent):
+        s = self.IndentString(indent) + str(self)
+        for c in self.childNodes:
+            s += c.TreeToString(indent + 1)
+        return s
+
+    def IndentString(self, indent):
+        s = "\n"
+        for i in range(1, indent + 1):
+            s += "| "
+        return s
+
+    def ChildrenToString(self):
+        s = ""
+        for c in self.childNodes:
+            s += str(c) + "\n"
+        return s
+
+
+def UCT(rootstate, itermax, verbose=False):
+    """ Conduct a UCT search for itermax iterations starting from rootstate.
+        Return the best move from the rootstate.
+        Assumes 2 alternating players (player 1 starts), with game results in the range [0.0, 1.0]."""
+
+    rootnode = Node(state=rootstate)
+
+    for i in range(itermax):
+        node = rootnode
+        state = rootstate.Clone()
+
+        # Select
+        while node.untriedMoves == [] and node.childNodes != []:  # node is fully expanded and non-terminal
+            node = node.UCTSelectChild()
+            state.DoMove(node.move)
+
+        # Expand
+        if node.untriedMoves != []:  # if we can expand (i.e. state/node is non-terminal)
+            m = random.choice(node.untriedMoves)
+            state.DoMove(m)
+            node = node.AddChild(m, state)  # add child and descend tree
+
+        # Rollout - this can often be made orders of magnitude quicker using a state.GetRandomMove() function
+        while state.GetMoves() != []:  # while state is non-terminal
+            state.DoMove(random.choice(state.GetMoves()))
+
+        # Backpropagate
+        while node != None:  # backpropagate from the expanded node and work back to the root node
+            node.Update(state.GetResult(
+                node.playerJustMoved))  # state is terminal. Update node with result from POV of node.playerJustMoved
+            node = node.parentNode
+
+    # Output some information about the tree - can be omitted
+    if (verbose):
+        print(rootnode.TreeToString(0))
+    else:
+        print(rootnode.ChildrenToString())
+
+    return sorted(rootnode.childNodes, key=lambda c: c.visits)[-1].move  # return the move that was most visited
--- ai_ai_gui_demo.py
+++ ai_ai_gui_demo.py
@@ -2,10 +2,12 @@ from goboard import GomokuGameHandler
 from goboard.judge import Win, Lose
 from goboard.logger import log
 from ai.normal_ai import Ai as NormalAi
+from ai.easy_ai import Ai as EasyAi
+from ai.group_23 import Ai as Group23Ai
 import time
 
 black_player = NormalAi("black", board_size=(13, 13))
-white_player = NormalAi("white", board_size=(13, 13))
+white_player = Group23Ai("white", board_size=(13, 13))
 
 try:
     with GomokuGameHandler(black_player, white_player, board_size=(13, 13)) as (black_round, white_round, board):
